363030c2dc84a68616402d7649b965042e45944a
BlueStreak82 <jraskr3@gmail.com>
2025-10-17 22:41:16 +0000

    Responsive Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Navbar } from './navbar/navbar';
+import { HeroSection } from './hero-section/hero-section';
+import { About } from './about/about';
+import { Portfolio } from './portfolio/portfolio';
+import { Contact } from './contact/contact';
+import { Footer } from './footer/footer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: '', component: HeroSection, },
+  // { path: 'navbar', component: Navbar },
+  // { path: 'about', component: About },
+  // { path: 'portfolio', component: Portfolio },
+  // { path: 'contact', component: Contact },
+  // { path: 'footer', component: Footer },
+];
+0
--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,20 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faBars } from '@fortawesome/free-solid-svg-icons';
+import { RouterLink } from '@angular/router';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [FontAwesomeModule],
+  standalone: true,
+  imports: [
+    FontAwesomeModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+  ],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+  styleUrls: ['./navbar.scss'],
 })
 export class Navbar {
+  @ViewChild('drawer') drawer!: MatSidenav;
+
   toAbout() {
     document.getElementById('about')?.scrollIntoView({ behavior: 'smooth' });
+    if (this.drawer.mode === 'over') {
+      this.drawer.close();
+    }
   }
 
   toSkills() {
     document.getElementById('skills')?.scrollIntoView({ behavior: 'smooth' });
+    if (this.drawer.mode === 'over') {
+      this.drawer.close();
+    }
   }
 
   toPortfolio() {
@@ -23,11 +45,18 @@ export class Navbar {
 
   toContact() {
     document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth' });
+    if (this.drawer.mode === 'over') {
+      this.drawer.close();
+    }
   }
 
   toHome() {
     document.getElementById('home')?.scrollIntoView({ behavior: 'smooth' });
+    if (this.drawer.mode === 'over') {
+      this.drawer.close();
+    }
   }
 
   faHome = faHome;
+  faBars = faBars;
 }
