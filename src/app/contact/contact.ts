e531bbcb94916e9dd0fa872d3a2d3e1248921687
BlueStreak82 <jraskr3@gmail.com>
2025-10-26 18:48:52 +0000

    Add EmailJS integration for contact form; update styles and error handling

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -5,12 +5,14 @@ import {
   Validators,
   FormGroup,
   FormControl,
+  NgForm,
 } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -25,6 +27,10 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './contact.scss',
 })
 export class Contact {
+  sending = false;
+  emailSent = false;
+  emailError = false;
+
   contactForm = new FormGroup({
     name: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -33,6 +39,37 @@ export class Contact {
   });
 
   onSubmit() {
-    console.log(this.contactForm.value);
+    // console.log(this.contactForm.value);
+
+    if (!this.contactForm.valid) return;
+    this.sending = true;
+
+    emailjs
+      .send(
+        'service_z1b6f5d', // Replace with your service ID
+        'template_1n5w06o', // Replace with your template ID
+        {
+          name: this.contactForm.value.name,
+          email: this.contactForm.value.email,
+          message: this.contactForm.value.message,
+          phoneNumber: this.contactForm.value.tel,
+        },
+        'p36eWslKvgpZkhePt' // Replace with your public key
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log('SUCCESS!', result.text);
+          this.emailSent = true;
+          this.emailError = false;
+          this.contactForm.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          this.emailError = true;
+        }
+      )
+      .finally(() => {
+        this.sending = false;
+      });
   }
 }
