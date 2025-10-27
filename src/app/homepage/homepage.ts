2c779509a661531414a57b1b4475a120fdcb481b
BlueStreak82 <jraskr3@gmail.com>
2025-10-27 13:17:53 +0000

    Remove unused import for HeroSection and clean up code formatting

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-
 import { Navbar } from '../navbar/navbar';
-import { HeroSection } from '../hero-section/hero-section';
 import { About } from '../about/about';
 import { Portfolio } from '../portfolio/portfolio';
 import { Contact } from '../contact/contact';
 import { Footer } from '../footer/footer';
 import { HeroSection2 } from '../hero-section2/hero-section2';
-import { MorePanel } from "../more-panel/more-panel";
+import { MorePanel } from '../more-panel/more-panel';
 
 @Component({
   selector: 'app-homepage',
